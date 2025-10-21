aef1b1986c5d5bc3b807747bd5b3422770a3b7dd
solaerius <eddie@ervenius.com>
2025-10-22 00:00:40 +0200

    base page #2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,18 @@ export default function Page() {
 
   return (
     <div className="min-h-[100vh] bg-zinc-950 text-zinc-100 selection:bg-emerald-500/30 selection:text-emerald-50">
-      
+      <header className="sticky top-0 z-30 backdrop-blur bg-zinc-950/60 border-b border-zinc-900">
+        <div className="max-w-6xl mx-auto flex items-center justify-between px-4 py-3 relative">
+          {/* Left side */}
+          <div className="flex items-center gap-3">
+            <div className="w-2.5 h-2.5 rounded full bg-emerald-400 animate-pulse"/>
+            <span className="font-mono text-sm text-zinc-300"> ~/eddie/dev-console</span>
+          </div>
+          
+          {/* command palatte */}
+          <div className="absolute left-1/2 -translate-x-1/2 w-[260px] sm:w-[340px]"></div>
+        </div>
+      </header>
     </div>
   )
 }
